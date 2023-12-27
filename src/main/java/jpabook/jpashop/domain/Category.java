d4579719d5dc9c8ededa95dd25a61e57b78a28a7
apfhd12gk <apfhd12gk@naver.com>
2023-12-27 16:29:21 +0900

    feat: Category Entity 생성

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany //실무에서 중간테이블을 만들어서 다대다를 쪼개어서 사용하자 ex) OrderItem 클래스처럼
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+
+}
